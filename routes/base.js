38c6411c3f1e96ff9b7ce383c95b3c2db97cff37
Anton Lapshin <antvallap@gmail.com>
2015-08-24 22:06:37 +0600

    Remove checking of auth

--- routes/base.js
+++ routes/base.js
@@ -4,26 +4,11 @@ var P = require('../p');
 exports.ERR_AUTH_FAIL = { message: "Authorization failed"};
 exports.MES_SUCCESS = { message: "Authorization successful"};
 
-var VK_APP_SECRET = "ESyZr9v9a8w2G8VZuDGz";
-var VK_APP_ID = "3955660";
-
 exports.auth = function (session, id, authKey) {
     return P.call(function (fulfill, reject) {
         if (session.player != undefined) {
             if (session.player.id == id) {
                 fulfill(exports.MES_SUCCESS);
-                return;
-            }
-        }
-
-        if (id !== 5653333)
-        {
-            var data = VK_APP_ID + '_' + id + '_' + VK_APP_SECRET;
-            var crypto = require('crypto');
-            var expectedAuthKey = crypto.createHash('md5').update(data).digest('hex');
-
-            if (authKey != expectedAuthKey) {
-                fulfill(exports.ERR_AUTH_FAIL);
                 return;
             }
         }
